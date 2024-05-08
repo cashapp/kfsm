18e5e7a53d0ded4c0b7c05456f41ecb3676bc748
Jeremy Mawson <jmawson@squareup.com>
2024-05-09 08:55:04 +1000

    Remove lib-arrow

--- lib-arrow/src/test/kotlin/com/squareup/cash/kfsm/exemplar/HamsterNotificationType.kt
+++
@@ -1,8 +0,0 @@
-package com.squareup.cash.kfsm.exemplar
-
-import com.squareup.cash.kfsm.NotificationType
-
-sealed class HamsterNotificationType : NotificationType
-object Console : HamsterNotificationType()
-object TheVoid : HamsterNotificationType()
-object Owner : HamsterNotificationType()
